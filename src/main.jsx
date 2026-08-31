e1ace17e36cc90f18742b3d9d8e63cb36fb09be7
Sajjad Hossain <sajjad00288@gmail.com>
2026-08-31 22:06:09 +0600

    Wire storefront and admin routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdminPanel from './components/AdminPanel.jsx'
 import './styles.css'
+import './commerce.css'
+
+const normalizedPath = window.location.pathname.replace(/\/+$/, '') || '/'
+const Root = normalizedPath === '/admin' ? AdminPanel : App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
