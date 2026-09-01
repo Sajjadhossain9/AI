47e1486a46927f433635ffd9eef72b9b3735a286
Sajjad Hossain <sajjad00288@gmail.com>
2026-09-02 03:25:16 +0600

    Route admin portal at /admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdminPanel from './components/AdminPanel.jsx'
 import './styles.css'
 import './commerce.css'
 import './simple-store.css'
+import './admin-portal.css'
+
+const normalizedPath = window.location.pathname.replace(/\/+$/, '') || '/'
+const Root = normalizedPath === '/admin' ? AdminPanel : App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
