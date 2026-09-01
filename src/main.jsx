eea070135fe469d7fea61468a3287fbe1096ccf8
Sajjad Hossain <sajjad00288@gmail.com>
2026-09-02 01:12:52 +0600

    Remove admin route and keep storefront only

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import AdminPanel from './components/AdminPanel.jsx'
 import './styles.css'
 import './commerce.css'
-
-const normalizedPath = window.location.pathname.replace(/\/+$/, '') || '/'
-const Root = normalizedPath === '/admin' ? AdminPanel : App
+import './simple-store.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>,
 )
